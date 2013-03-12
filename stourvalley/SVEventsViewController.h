abd4d848ce74b854930629b6f57487f48f427b6b
Bassem Shaker <bassemreda@gmail.com>
2013-03-12 13:36:24 +0000

    added About, Events, and Map view controllers

---
+++ stourvalley/SVEventsViewController.h
@@ -0,0 +1,13 @@
+//
+//  SVEventsViewController.h
+//  stourvalley
+//
+//  Created by Bassem on 12/03/2013.
+//  Copyright (c) 2013 Bassem Shaker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SVEventsViewController : UIViewController
+
+@end
---
+++ stourvalley/SVMapViewController.h
@@ -0,0 +1,13 @@
+//
+//  SVMapViewController.h
+//  stourvalley
+//
+//  Created by Bassem on 12/03/2013.
+//  Copyright (c) 2013 Bassem Shaker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SVMapViewController : UIViewController
+
+@end
