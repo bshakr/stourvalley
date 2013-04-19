f2001d3fa1f869463f8da9d9f1e1913e537a145d
Bassem Shaker <bassemreda@gmail.com>
2013-04-19 01:39:19 +0100

    added mapbox and SVMapboxViewController

---
+++ Pods/Pods-header.h
@@ -0,0 +1,11 @@
+// WARNING: This feature of CocoaPods is present for discussion purposes and might be discontinued or changed in future
+#define __COCOA_PODS
+
+#define __POD_FMDB
+#define __POD_FMDB_common
+#define __POD_FMDB_standard
+#define __POD_GRMustache
+#define __POD_JRSwizzle
+#define __POD_MapBox
+#define __POD_NVSlideMenuController
+#define __POD_SMCalloutView
---
+++ stourvalley/SVMapboxViewController.h
@@ -0,0 +1,14 @@
+//
+//  SVMapboxViewController.h
+//  stourvalley
+//
+//  Created by Bassem on 19/04/2013.
+//  Copyright (c) 2013 Bassem Shaker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreLocation/CoreLocation.h>
+
+@interface SVMapboxViewController : UIViewController
+
+@end
